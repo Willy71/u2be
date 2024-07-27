74626d8062b9f3bb8cf5aaa28d2e5d8d38e55205
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-27 18:27:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("Reproductor de Youtube sem publicidade")
 
-video = st.text_input("Cole o endereço de Youtube")
+video = st.text_input("Cole o endereço de Youtube", "")
 
 st.video(video, autoplay=True)
 
