e1e378240242fab5c9915415c1b71bda390ccadf
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-07-29 16:53:13 -0300

    Update busqueda.py

--- paginas/busqueda.py
+++ paginas/busqueda.py
@@ -1,1 +1,14 @@
+import streamlit as st
 
+# Colocar nome na pagina, icone e ampliar a tela
+st.set_page_config(
+    page_title="Busquedas",
+    page_icon=":)",
+    layout="wide"
+)
+
+with st.container():
+    col01, col02, col03 = st.columns(3)
+    with col01:
+        if st.button("Anterior", use_container_width=True):
+            st.switch_page("app.py")
