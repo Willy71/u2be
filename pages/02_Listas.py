7e25550d0d6c358ccf5d128a721c3e4d33f78ee8
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-08-02 18:57:12 -0300

    Update 02_Listas.py

--- pages/02_Listas.py
+++ pages/02_Listas.py
@@ -45,7 +45,7 @@ def get_videos(playlist_id):
     return response['items']
 
 def main():
-    centrar_texto("❤ ❤ SamyTube Player ❤ ❤", 2, 'pink')
+    #centrar_texto("❤ ❤ SamyTube Player ❤ ❤", 2, 'pink')
     #st.sidebar.title("Opciones")
 
     # Entradas de texto para la API key y el channel ID
